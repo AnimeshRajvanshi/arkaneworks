10d31e54892984083d577255c238b37001bae215
AnimeshRajvanshi <rajvanshianimesh@gmail.com>
2025-03-17 16:27:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let menuButton = document.querySelector('.menu-button');
-    let navigation = document.querySelector('.navigation');
-    menuButton.addEventListener('click', function() {
-        if (navigation.style.left === '-200px' || navigation.style.left === '') {
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.menu-button');
+    const navigation = document.querySelector('.navigation');
+    const pageContainer = document.querySelector('.page-container');
+    let isMenuOpen = false;
+
+    menuButton.addEventListener('click', () => {
+        isMenuOpen = !isMenuOpen;
+        if (isMenuOpen) {
             navigation.style.left = '0';
+            pageContainer.classList.add('menu-open');
         } else {
             navigation.style.left = '-200px';
+            pageContainer.classList.remove('menu-open');
         }
     });
 });
