67859a6823ceb171de667016cd8a34ea386a3637
AnimeshRajvanshi <rajvanshianimesh@gmail.com>
2025-05-01 21:55:03 -0700

    Create wrongway.js

---
+++ animations/wrongway.js
@@ -0,0 +1,1 @@
+initAnimation('animation-canvas', '/assets/wrongway_frames/frame_', 120, () => document.querySelector('.video-background').offsetHeight, () => document.getElementById('credits').offsetTop, 200, 200);
